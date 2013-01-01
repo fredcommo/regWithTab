d3a5f341ed915160553ee987b57eea2078c5cbf4
FredCommo <fredcommo@gmail.com>
2013-01-01 11:37:44 -0800

    a simple graph

--- simpleRcode.R
+++ simpleRcode.R
@@ -1,9 +1,11 @@
 # A really simple code
 
-x = seq(1, 20)
-y = 2*x + rnorm(length(x))
+n = 100
+x = seq(1, n)
+y = 2*x + rnorm(n, 0, 5)
 
 lm1 <- lm(y~x)
 summary(lm1)
+X11(type = 'dbcairo')
 plot(x, y, pch = 19, col = 'grey')
 abline(lm1, col = 'blue')
